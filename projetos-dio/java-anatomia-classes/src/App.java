f878dd6b0c2beed505aeba553e60b795cca4e82f
MarlonHDC <marlon_hdc@hotmail.com>
2024-12-10 09:49:29 -0300

    initial commit

---
+++ projetos-dio/java-anatomia-classes/src/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Entrada do serviço a ser verificado
+        String servico = scanner.nextLine().trim();
+        
+        // Entrada do nome do cliente e os serviços contratados
+        String entradaCliente = scanner.nextLine().trim();
+        
+        // Separando o nome do cliente e os serviços contratados
+        String[] partes = entradaCliente.split(",");
+        String nomeCliente = partes[0];
+        String nomeServico = partes[1];
+        boolean contratado = false;
+        
+        // TODO: Verifique se o serviço está na lista de serviços contratados
+        if(nomeServico  == servico) 
+        {
+          System.out.println( "Sim");
+        }
+        else
+        {
+          System.out.println("Nao");
+        }
+        scanner.close();
+    }
+}
